08ae704e8264efc3a3f2cec17bf027fba19349c7
Akom4n <xdenik13@gmail.com>
2022-10-05 15:53:14 -0300

    projetos dio

---
+++ src/Exercicios_sobrecarga/Sobrecarga_main.java
@@ -0,0 +1,13 @@
+package Exercicios_sobrecarga;
+
+public class Sobrecarga_main {
+
+    public static void main(String[] args) {
+
+        // Quadrilátero
+        System.out.println("Exercício quadrilatero");
+        Quadrilatero.area(3);
+        Quadrilatero.area(5, 5);
+        Quadrilatero.area(7, 8, 9);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,7 @@
 public class Main {
+
     public static void main(String[] args) {
 
-        double d1;
-        float f1 = 10.5f;
-        d1 = f1;
-
-        System.out.println(d1);
     }
+
 }
