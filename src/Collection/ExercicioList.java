b862e4f615a52550f0998713ae6544280d7f719c
Akom4n <xdenik13@gmail.com>
2022-10-17 15:12:46 -0300

    ATT

--- src/Collection/ExercicioList.java
+++ src/Collection/ExercicioList.java
@@ -23,6 +23,32 @@ public class ExercicioList {
 		
 		
 		System.out.println("Crie uma lista e adicione as sete notas: ");
+		List<Double> notas = new ArrayList<>();
+		notas.add(7.0);
+		notas.add(8.5);
+		notas.add(9.3);
+		notas.add(5.0);
+		notas.add(7.0);
+		notas.add(8.0);
+		notas.add(3.6);
+		System.out.println(notas.toString());
+		
+		System.out.println("Exiba a posição da nota 5.0: " + notas.indexOf(5.0));
+		
+		System.out.println("Adicione na lista a nota 8.0 na posição 4: ");
+		notas.add(4, 8.0);
+		System.out.println(notas);
+		
+		System.out.println("Substitua a nota 5.0 pela nota 6.0: ");
+		notas.set(notas.indexOf(5.0),6.0);
+		System.out.println(notas);
+		
+		System.out.println("Confira se a nota 5.0 está na lista: " + notas.contains(5d));
+		
+		
+		//System.out.println("Exibe todas as notas na ordem em que foram informados: ");
+		//for (Double nota : notas) System.out.println(nota);
+		
 		
 	}
 
