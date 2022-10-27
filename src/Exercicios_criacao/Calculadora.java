9e1d4f93f8c641dc5657fc969474ca6607953b42
Akom4n <xdenik13@gmail.com>
2022-10-27 17:22:26 -0300

    att

--- src/Exercicios_criacao/Calculadora.java
+++ src/Exercicios_criacao/Calculadora.java
@@ -12,7 +12,7 @@ public class Calculadora {
 
     public static void subtracao(double numero1, double numero2) {
 
-        double resultado = numero1 - numero2;s
+        double resultado = numero1 - numero2;
 
         System.out.println("A subtração de " + numero1 + " - " + numero2 + " é " + resultado);
     }
