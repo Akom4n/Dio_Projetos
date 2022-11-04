b475e4e950818830bed78adc236195873386c61b
Vinícius Silva <xdenik13@gmail.com>
2022-11-03 22:35:20 -0300

    ATT
    exemplos de tratamento de exceções.

--- src/exceptions/CheckedException.java
+++ src/exceptions/CheckedException.java
@@ -8,21 +8,22 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 
+import javax.swing.JOptionPane;
+
 public class CheckedException {
 	
-	public static void main(String[] args) throws IOException {
-		String nomeDoArquivo = "romances-blake-crouch.txt";
-		imprimirArquivoNoConsole(nomeDoArquivo);
+	public static void main(String[] args) {
+		String nomeDoArquivo = JOptionPane.showInputDialog("Nome do arquivo a ser exibido: ");
 		
-		System.out.println("Apesar da exception ou não, o programa continua...");
+		imprimirArquivoNoConsole(nomeDoArquivo);
+		System.out.println("\nCom exception ou não, o programa continua...");
 	}
 	
-	public static void imprimirArquivoNoConsole(String nomeDoArquivo) throws IOException{
-		File file = new File(nomeDoArquivo);
+	public static void imprimirArquivoNoConsole(String nomeDoArquivo){
 		
-		BufferedReader br = new BufferedReader(new FileReader(file.getName()));	
+		try {
+		BufferedReader br = lerArquivo(nomeDoArquivo);
 		String line = br.readLine(); 
-		
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		do {
@@ -30,9 +31,42 @@ public class CheckedException {
 			bw.newLine();
 			line=br.readLine();
 		} while(line != null);
-		bw.flush();
-		br.close();
-		
+			bw.flush();
+			br.close();
+		}catch (ImpossivelAberturaDeArquivoException e) {
+			JOptionPane.showMessageDialog(null, e.getMessage());
+			e.printStackTrace();
+		} catch(IOException ex) {
+			JOptionPane.showMessageDialog(null, "Ocorreu um erro inesperado! Entre em contato com o suporte! " + ex.getMessage());
+			ex.printStackTrace();
+		}
 	}
+	
+	public static BufferedReader lerArquivo(String nomeDoArquivo) throws ImpossivelAberturaDeArquivoException {
+		File file = new File(nomeDoArquivo);
+		try {
+			return new BufferedReader(new FileReader(nomeDoArquivo));
+		}catch(FileNotFoundException e){
+			throw new ImpossivelAberturaDeArquivoException(file.getName(), file.getPath());
+		}
+	}
+}
 
+class ImpossivelAberturaDeArquivoException extends Exception {
+	
+	private String nomeDoArquivo;
+	private String diretorio;
+	
+	public ImpossivelAberturaDeArquivoException (String nomeDoArquivo, String diretorio){
+		super("O arquivo " + nomeDoArquivo + " não foi encontrado no diretório " + diretorio);
+		this.nomeDoArquivo = nomeDoArquivo;
+		this.diretorio = diretorio;
+	}
+	
+	public String toString() {
+		return "ImpossivelAberturaDeArquivoException{" +
+				"nomeDoArquivo='" + nomeDoArquivo + '\'' +
+				", diretorio='" + diretorio + '\'' + '}';
+	}
+	
 }
